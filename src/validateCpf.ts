eec94e9f36cf2a39cdf57626dcb6dc3a8a9e1f64
bcmes <brunomeloesilva@gmail.com>
2024-04-16 21:18:38 -0400

    try-catch inalcançável

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -26,35 +26,30 @@ export function validate(str: string) {
     str = clean(str);
     if (!isValidLength(str)) return false;
     if (!allDigitsEquals(str)) {
-        try {
-            let d1, d2;
-            let dg1, dg2, rest;
-            let digito;
-            let nDigResult;
-            d1 = d2 = 0;
-            dg1 = dg2 = rest = 0;
-            for (let nCount = 1; nCount < str.length - 1; nCount++) {
-                digito = parseInt(str.substring(nCount - 1, nCount));
-                d1 = d1 + (11 - nCount) * digito;
-                d2 = d2 + (12 - nCount) * digito;
-            };
-            rest = (d1 % 11);
-            // se for menor que 2 é 0, senão é 11 menos o resto
-            dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-            d2 += 2 * dg1;
-            rest = (d2 % 11);
-            if (rest < 2)
-                dg2 = 0;
-            else
-                dg2 = 11 - rest;
-            let nDigVerific = str.substring(str.length - 2, str.length);
-            nDigResult = "" + dg1 + "" + dg2;
-            return nDigVerific == nDigResult;
-            // se der problema...
-        } catch (e) {
-            console.error("Erro !" + e);
-            return false;
-        }
+        let d1, d2;
+        let dg1, dg2, rest;
+        let digito;
+        let nDigResult;
+        d1 = d2 = 0;
+        dg1 = dg2 = rest = 0;
+        for (let nCount = 1; nCount < str.length - 1; nCount++) {
+            digito = parseInt(str.substring(nCount - 1, nCount));
+            d1 = d1 + (11 - nCount) * digito;
+            d2 = d2 + (12 - nCount) * digito;
+        };
+        rest = (d1 % 11);
+        // se for menor que 2 é 0, senão é 11 menos o resto
+        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
+        d2 += 2 * dg1;
+        rest = (d2 % 11);
+        if (rest < 2)
+            dg2 = 0;
+        else
+            dg2 = 11 - rest;
+        let nDigVerific = str.substring(str.length - 2, str.length);
+        nDigResult = "" + dg1 + "" + dg2;
+        return nDigVerific == nDigResult;
+        // se der problema...
     } else return false;
 
     function isValidLength(cpf: string) {
