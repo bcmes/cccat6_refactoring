0a28b069737b9f149d8960ed26ed59ce1afee844
bcmes <brunomeloesilva@gmail.com>
2024-04-17 06:53:07 -0400

    remocao de comentario

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -1,26 +1,3 @@
-/*Pontos de refatoração:
-- linhas em branco dentro de método, delete, e aumente o line hight da IDE.
-- código morto: código comentado, simplesmente apague, é pra isso que existe git. Se for usar de vez enguando, faz feature-flag.
-- nome sem sentido com o proposito do processo a ser resolvido, a começar pela Classe, método e seus membros internos (variáveis, etc..)
-- não misturar portugues com english
-- comentários tentando exclarecer o código obscuro. Retire o comentario e refatore, com funcoes internas, etc..
-- numeros mágicos, crie constantes.
-- fragmento de código duplicado, extraia para um método.
-- variaveis declaradas longe da utilizacao. Declare próximo do uso.
-- condições confusas, apresentadas por aninhamentos, ficando longe da marge. Resolva negando primeiro, chamada de clausula guarda.
-- um if-else com atribuição, deve-se usar ternário. Nunca use ternários aninhados, nesse caso volta pra if.
--> Obs.: Só é seguro refatorar com a pré-existência dos testes.
--> Obs.: Cobertura é diferente de validação de comportamento
--> Obs.: Teste de unidade é o teste de uma entidade ( um trecho de código que é independente de recursos, que tem regras isoladas, geralmente do dominio )
--> Obs.: Given When Then precisam estar claramente presentes, mas nao precisam estar didaticamente separados
--> Obs.: Testes devem:
-    - Rápido: Se executar o mais rápido possível
-    - Idependentes: Ser completamente idenpedentes uns dos outros
-    - Repetivel: Se executado N vezes repetidamente, devem dar o mesmo resultado
-    - Auto-Validado: Ter boas validações
-    - Timely: Idealmente devem ser escritos antes do código
-*/
-
 const FACTOR_FIRST_DIGIT = 10;
 const FACTOR_SECOND_DIGIT = 11;
 
